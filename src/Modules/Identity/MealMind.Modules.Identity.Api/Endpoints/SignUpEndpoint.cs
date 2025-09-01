ab8d4bbf3a4ddb5fbcf0cc0745a2564ee39d2436
michaldrozd <michaldrozd03@gmail.com>
2025-09-01 19:01:44 +0200

    Addes SignUpEndpoint

---
+++ src/Modules/Identity/MealMind.Modules.Identity.Api/Endpoints/SignUpEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using MealMind.Modules.Identity.Application.Features.Commands.SignUpCommand;
+using MealMind.Shared.Abstractions.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Stoxly.Shared.Abstractions.Api;
+
+namespace MealMind.Modules.Identity.Api.Endpoints;
+
+internal sealed class SignUpEndpoint : EndpointBase
+{
+    public override void AddEndpoint(IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        endpointRouteBuilder.MapPost("sign-up",
+                async (SignUpCommand request, ISender sender) =>
+                {
+                    var result = await sender.Send(request);
+                    return Results.Ok(result);
+                })
+            .AllowAnonymous();
+    }
+}
