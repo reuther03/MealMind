b34177a3ed66620ca2f4f22c82c1288c2ed7c7c8
michaldrozd <michaldrozd03@gmail.com>
2025-09-05 19:28:51 +0200

    Adds endpoint documentation extension method

--- src/Shared/MealMind.Shared.Abstractions/Api/EndpointBaseExtensions.cs
+++ src/Shared/MealMind.Shared.Abstractions/Api/EndpointBaseExtensions.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
 
 namespace MealMind.Shared.Abstractions.Api;
 
@@ -8,12 +13,48 @@ public static class EndpointBaseExtensions
     public static RouteHandlerBuilder WithDocumentation(
         this RouteHandlerBuilder builder,
         string name,
-        string summary,
-        string description
+        string description,
+        [StringSyntax(StringSyntaxAttribute.Json)]
+        string requestExample,
+        [StringSyntax(StringSyntaxAttribute.Json)]
+        string responseExample
     )
         => builder
-            .WithName(name)
-            .WithSummary(summary)
-            .WithDescription(description)
+            .WithName(name) // Sets the endpoint name displayed in Swagger
+            .WithSummary(description) // Sets the summary shown at the top of the endpoint
+            .WithDescription(CreateDescription(name, description, requestExample, responseExample)) // Sets the description (using summary for consistency)
             .WithOpenApi();
+
+
+    private static string CreateDescription(string name, string description, string requestExample, string responseExample)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"**{name}**");
+        sb.AppendLine();
+        sb.AppendLine($"**{description}**");
+
+        if (!string.IsNullOrWhiteSpace(requestExample))
+        {
+            sb.AppendLine();
+            sb.AppendLine("---");
+            sb.AppendLine();
+            sb.AppendLine("**Request Example:**");
+            sb.AppendLine("```json");
+            sb.AppendLine(requestExample);
+            sb.AppendLine("```");
+        }
+
+        if (!string.IsNullOrWhiteSpace(responseExample))
+        {
+            sb.AppendLine();
+            sb.AppendLine("---");
+            sb.AppendLine();
+            sb.AppendLine("**Response Example:**");
+            sb.AppendLine("```json");
+            sb.AppendLine(responseExample);
+            sb.AppendLine("```");
+        }
+
+        return sb.ToString();
+    }
 }
