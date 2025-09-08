fb20bc8ab1d854deb6c8200f6e71c51bd8f1facd
michaldrozd <michaldrozd03@gmail.com>
2025-09-08 19:09:11 +0200

    Adds initial tracking domain entities

---
+++ src/Modules/Nutrition/MealMind.Modules.Nutrition.Domain/Tracking/DailyLog.cs
@@ -0,0 +1,9 @@
+﻿using MealMind.Shared.Abstractions.Kernel.Primitives;
+
+namespace MealMind.Modules.Nutrition.Domain.Tracking;
+
+public class DailyLog : Entity<Guid>
+{
+    public DateTime StartDate { get; private set; }
+    public decimal CurrentWeight { get; private set; }
+}
