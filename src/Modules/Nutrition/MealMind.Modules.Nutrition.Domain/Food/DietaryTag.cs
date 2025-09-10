1bbcf499cafaa9a5446c788e6a9a832b6ab98457
michaldrozd <michaldrozd03@gmail.com>
2025-09-10 03:09:46 +0200

    Adds Food and Dietary enums

---
+++ src/Modules/Nutrition/MealMind.Modules.Nutrition.Domain/Food/DietaryTag.cs
@@ -0,0 +1,36 @@
+﻿namespace MealMind.Modules.Nutrition.Domain.Food;
+
+public enum DietaryTag
+{
+    // Diet Preferences
+    Vegan = 1, // No animal products
+    Vegetarian, // No meat, but may include dairy/eggs
+    Pescatarian, // Fish allowed, no other meat
+    Keto, // Low-carb, high-fat
+    Paleo, // Whole foods, no processed grains/dairy
+    LowCarb, // Reduced carbohydrates
+    HighProtein, // Focus on protein-rich foods
+    LowFat, // Reduced fat content
+    LowCalorie, // Suitable for calorie-restricted diets
+
+    // Allergen-Free Options
+    GlutenFree,
+    DairyFree,
+    LactoseFree,
+    NutFree,
+    PeanutFree,
+    SoyFree,
+    EggFree,
+    ShellfishFree,
+    FishFree,
+    SesameFree,
+
+    // Other Lifestyle Preferences
+    Organic,
+    NonGmo,
+    SugarFree,
+    LowSugar,
+    CaffeineFree,
+    WholeGrain,
+    HighFiber,
+}
