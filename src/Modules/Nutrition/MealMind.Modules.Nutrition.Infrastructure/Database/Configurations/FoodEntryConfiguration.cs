02663787366122c3ee652f02a86376658d2b52cd
michaldrozd <michaldrozd03@gmail.com>
2025-09-16 02:44:05 +0200

    Configures database entities for Nutrition module

---
+++ src/Modules/Nutrition/MealMind.Modules.Nutrition.Infrastructure/Database/Configurations/FoodEntryConfiguration.cs
@@ -0,0 +1,69 @@
+﻿using MealMind.Modules.Nutrition.Domain.Food;
+using MealMind.Modules.Nutrition.Domain.Tracking;
+using MealMind.Shared.Abstractions.Kernel.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MealMind.Modules.Nutrition.Infrastructure.Database.Configurations;
+
+public class FoodEntryConfiguration : IEntityTypeConfiguration<FoodEntry>
+{
+    public void Configure(EntityTypeBuilder<FoodEntry> builder)
+    {
+        builder.ToTable("FoodEntry");
+
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        builder.Property(x => x.FoodId)
+            .HasConversion(x => x.Value, x => FoodId.From(x))
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(x => x.FoodName)
+            .HasConversion(x => x.Value, x => new Name(x))
+            .IsRequired();
+
+        builder.Property(x => x.FoodBrand)
+            .HasConversion(x => x!.Value, x => new Name(x))
+            .HasMaxLength(100)
+            .IsRequired(false);
+
+        builder.Property(x => x.QuantityInGrams)
+            .HasPrecision(8, 2)
+            .IsRequired();
+
+        builder.Property(x => x.TotalCalories)
+            .HasPrecision(8, 2)
+            .IsRequired();
+
+        builder.Property(x => x.TotalProteins)
+            .HasPrecision(6, 2)
+            .IsRequired();
+
+        builder.Property(x => x.TotalCarbohydrates)
+            .HasPrecision(6, 2)
+            .IsRequired();
+
+        builder.Property(x => x.TotalSugars)
+            .HasPrecision(6, 2)
+            .IsRequired();
+
+        builder.Property(x => x.TotalFats)
+            .HasPrecision(6, 2)
+            .IsRequired();
+
+        builder.Property(x => x.TotalSaturatedFats)
+            .HasPrecision(6, 2)
+            .IsRequired();
+
+        builder.Property(x => x.TotalFiber)
+            .HasPrecision(6, 2)
+            .IsRequired();
+
+        builder.Property(x => x.TotalSodium)
+            .HasPrecision(8, 2)
+            .IsRequired();
+    }
+}
---
+++ src/Modules/Nutrition/MealMind.Modules.Nutrition.Infrastructure/Database/Configurations/MealConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using MealMind.Modules.Nutrition.Domain.Tracking;
+using MealMind.Shared.Abstractions.Kernel.ValueObjects;
+using MealMind.Shared.Abstractions.Kernel.ValueObjects.Ids;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MealMind.Modules.Nutrition.Infrastructure.Database.Configurations;
+
+public class MealConfiguration : IEntityTypeConfiguration<Meal>
+{
+    public void Configure(EntityTypeBuilder<Meal> builder)
+    {
+        builder.ToTable("Meal");
+
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        builder.Property(x => x.MealType)
+            .HasConversion<string>()
+            .IsRequired();
+
+        builder.Property(x => x.Name)
+            .HasConversion(x => x!.Value, x => new Name(x))
+            .IsRequired();
+
+        builder.Property(x => x.UserId)
+            .HasConversion(x => x.Value, x => UserId.From(x))
+            .IsRequired();
+
+        builder.Property(x => x.ConsumedAt)
+            .IsRequired(false);
+
+        builder.Property(x => x.Notes)
+            .HasMaxLength(500)
+            .IsRequired(false);
+
+        builder.HasMany(x => x.Foods)
+            .WithOne()
+            .HasForeignKey("MealId")
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
