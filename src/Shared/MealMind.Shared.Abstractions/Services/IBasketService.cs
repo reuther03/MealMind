9974a0beb1567d9d7172cf4ee7d8b60ae1df1faf
michaldrozd <michaldrozd03@gmail.com>
2025-09-20 02:55:48 +0200

    Removes unused service abstractions

--- src/Shared/MealMind.Shared.Abstractions/Services/IBasketService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MealMind.Shared.Abstractions.Services;
-
-public interface IBasketService
-{
-    // Task<IList<Guid>> GetBasketItemsAsync(Guid userId, CancellationToken cancellationToken);
-}
--- src/Shared/MealMind.Shared.Abstractions/Services/IShippingService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MealMind.Shared.Abstractions.Services;
-
-public interface IShippingService
-{
-    // Task<ShipmentDto> CreateShipmentAsync(CreateShipmentCommand cmd);
-    // Task<IEnumerable<RateDto>> GetRatesAsync(Guid shipmentId);
-    // Task<LabelDto> PurchaseLabelAsync(PurchaseLabelCommand cmd);
-    // Task<TrackingInfoDto> GetTrackingAsync(Guid shipmentId);
-}
