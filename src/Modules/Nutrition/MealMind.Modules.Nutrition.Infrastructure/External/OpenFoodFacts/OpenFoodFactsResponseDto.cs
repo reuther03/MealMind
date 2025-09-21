8c3ac575725d6c9844f50c3910568675dd7a4685
michaldrozd <michaldrozd03@gmail.com>
2025-09-22 01:42:35 +0200

    Adds Open Food Facts response DTO

---
+++ src/Modules/Nutrition/MealMind.Modules.Nutrition.Infrastructure/External/OpenFoodFacts/OpenFoodFactsResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MealMind.Modules.Nutrition.Infrastructure.External.OpenFoodFacts;
+
+public class OpenFoodFactsResponseDto
+{
+    [JsonPropertyName("count")]
+    public int Count { get; init; } // Total number of products found
+
+    [JsonPropertyName("page")]
+    public int Page { get; init; }
+
+    [JsonPropertyName("page_count")]
+    public int PageCount { get; init; }
+
+    [JsonPropertyName("page_size")]
+    public int PageSize { get; init; }
+
+    [JsonPropertyName("products")]
+    public List<OpenFoodFactsDto>? Products { get; init; }
+}
