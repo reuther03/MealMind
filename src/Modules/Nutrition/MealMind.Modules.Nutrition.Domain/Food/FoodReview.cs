05220a45fc5675ba7b88b67c42fd422b22d7b42b
michaldrozd <michaldrozd03@gmail.com>
2025-09-26 01:45:19 +0200

    Adds food review functionality

---
+++ src/Modules/Nutrition/MealMind.Modules.Nutrition.Domain/Food/FoodReview.cs
@@ -0,0 +1,32 @@
+﻿using MealMind.Shared.Abstractions.Exception;
+using MealMind.Shared.Abstractions.Kernel.Primitives;
+using MealMind.Shared.Abstractions.Kernel.ValueObjects.Ids;
+
+namespace MealMind.Modules.Nutrition.Domain.Food;
+
+public class FoodReview : Entity<Guid>
+{
+    public FoodId FoodId { get; private set; }
+    public UserId UserId { get; private set; }
+    public int Rating { get; private set; }
+    public string? Comment { get; private set; }
+
+    private FoodReview()
+    {
+    }
+
+    private FoodReview(Guid id, FoodId foodId, UserId userId, int rating, string? comment) : base(id)
+    {
+        FoodId = foodId;
+        UserId = userId;
+        Rating = rating;
+        Comment = comment;
+    }
+
+    public static FoodReview Create(Guid id, FoodId foodId, UserId userId, int rating, string? comment)
+    {
+        return rating is < 1 or > 5
+            ? throw new DomainException("Rating must be between 1 and 5.", nameof(rating))
+            : new FoodReview(id, foodId, userId, rating, comment);
+    }
+}
---
+++ src/Modules/Nutrition/MealMind.Modules.Nutrition.Infrastructure/Database/Configurations/FoodReviewConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using MealMind.Modules.Nutrition.Domain.Food;
+using MealMind.Shared.Abstractions.Kernel.ValueObjects.Ids;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MealMind.Modules.Nutrition.Infrastructure.Database.Configurations;
+
+public class FoodReviewConfiguration : IEntityTypeConfiguration<FoodReview>
+{
+    public void Configure(EntityTypeBuilder<FoodReview> builder)
+    {
+        builder.ToTable("FoodReviews");
+
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        builder.Property(x => x.FoodId)
+            .HasConversion(x => x.Value, x => FoodId.From(x))
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(x => x.UserId)
+            .HasConversion(x => x.Value, x => UserId.From(x))
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(x => x.Rating)
+            .IsRequired();
+
+        builder.Property(x => x.Comment)
+            .HasMaxLength(250)
+            .IsRequired(false);
+
+        builder.HasIndex(x => new { x.FoodId, x.UserId })
+            .IsUnique();
+
+        builder.HasOne<Food>()
+            .WithMany()
+            .HasForeignKey(x => x.FoodId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
