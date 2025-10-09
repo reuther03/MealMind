2985ee379d8d5fe95e842df0a00ef27f6ebae6c0
michaldrozd <michaldrozd03@gmail.com>
2025-10-09 19:16:27 +0200

    Addes AiChatService

---
+++ src/Services/MealMind.Services.AiChat/AiChatModule.cs
@@ -0,0 +1,24 @@
+﻿// ReSharper disable ClassNeverInstantiated.Global
+
+using MealMind.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MealMind.Services.AiChat;
+
+public class AIChatModule : IModule
+{
+    public const string BasePath = "AIChat-module";
+
+    public string Name => "AIChat";
+    public string Path => BasePath;
+
+    public void Register(IServiceCollection services)
+    {
+        services.AddAiChat();
+    }
+
+    public void Use(WebApplication app)
+    {
+    }
+}
---
+++ src/Services/MealMind.Services.AiChat/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MealMind.Services.AiChat;
+
+public static class Extensions
+{
+    public static IServiceCollection AddAiChat(this IServiceCollection services)
+    {
+        return services;
+    }
+}
