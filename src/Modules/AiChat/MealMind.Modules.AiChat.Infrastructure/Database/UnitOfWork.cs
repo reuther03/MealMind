23a44af1088af2709a0b2790776fa55609163adc
michaldrozd <michaldrozd03@gmail.com>
2025-10-11 02:03:12 +0200

    Adds Aichat UnitOfWork

---
+++ src/Modules/AiChat/MealMind.Modules.AiChat.Infrastructure/Database/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using MealMind.Modules.AiChat.Application.Abstractions;
+using MealMind.Shared.Abstractions.Services;
+using MealMind.Shared.Infrastructure.Postgres;
+
+namespace MealMind.Modules.AiChat.Infrastructure.Database;
+
+internal class UnitOfWork : BaseUnitOfWork<AiChatDbContext>, IUnitOfWork
+{
+    public UnitOfWork(AiChatDbContext context, IPublisher publisher) : base(context, publisher)
+    {
+    }
+}
