c8ab39f188ddd4fd08883a69f309c4da3068905e
michaldrozd <michaldrozd03@gmail.com>
2025-10-12 17:14:19 +0200

    Addes conversation repository

---
+++ src/Modules/AiChat/MealMind.Modules.AiChat.Application/Abstractions/Database/IConversationRepository.cs
@@ -0,0 +1,9 @@
+﻿using MealMind.Modules.AiChat.Domain.Conversation;
+using MealMind.Shared.Abstractions.Kernel.Database;
+
+namespace MealMind.Modules.AiChat.Application.Abstractions.Database;
+
+public interface IConversationRepository : IRepository<Conversation>
+{
+
+}
---
+++ src/Modules/AiChat/MealMind.Modules.AiChat.Infrastructure/Database/Repositories/ConversationRepository.cs
@@ -0,0 +1,12 @@
+﻿using MealMind.Modules.AiChat.Application.Abstractions.Database;
+using MealMind.Modules.AiChat.Domain.Conversation;
+using MealMind.Shared.Infrastructure.Postgres;
+
+namespace MealMind.Modules.AiChat.Infrastructure.Database.Repositories;
+
+internal class ConversationRepository : Repository<Conversation, AiChatDbContext>, IConversationRepository
+{
+    public ConversationRepository(AiChatDbContext dbContext) : base(dbContext)
+    {
+    }
+}
