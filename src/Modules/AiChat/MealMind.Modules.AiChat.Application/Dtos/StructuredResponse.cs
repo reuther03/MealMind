0ecca50ec3ce6e08469b9e244aa7e71c93cd1a80
michaldrozd <michaldrozd03@gmail.com>
2025-10-22 15:06:27 +0200

    Defines structured response data transfer object

--- src/Modules/AiChat/MealMind.Modules.AiChat.Application/Dtos/StructuredResponse.cs
+++ src/Modules/AiChat/MealMind.Modules.AiChat.Application/Dtos/StructuredResponse.cs
@@ -1,6 +1,9 @@
 ﻿namespace MealMind.Modules.AiChat.Application.Dtos;
 
-public class StructuredResponse
+public sealed record StructuredResponse
 {
-    
+    public string Title { get; init; } = string.Empty;
+    public List<string> Paragraphs { get; init; } = [];
+    public List<string> KeyPoints { get; init; } = [];
+    public List<string> Sources { get; init; } = [];
 }
