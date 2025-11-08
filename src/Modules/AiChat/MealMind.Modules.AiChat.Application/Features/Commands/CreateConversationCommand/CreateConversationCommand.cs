ce2ad1a9fdadd4bbc6e1539ce7c421dfa8281c9d
michaldrozd <michaldrozd03@gmail.com>
2025-11-08 19:18:32 +0100

    Implements structured AI chat responses for CreateConversationCommand

--- src/Modules/AiChat/MealMind.Modules.AiChat.Application/Features/Commands/CreateConversationCommand/CreateConversationCommand.cs
+++ src/Modules/AiChat/MealMind.Modules.AiChat.Application/Features/Commands/CreateConversationCommand/CreateConversationCommand.cs
@@ -1,68 +1,118 @@
-﻿using MealMind.Modules.AiChat.Application.Abstractions;
+﻿using System.Text.Json;
+using MealMind.Modules.AiChat.Application.Abstractions;
 using MealMind.Modules.AiChat.Application.Abstractions.Database;
+using MealMind.Modules.AiChat.Application.Abstractions.Services;
+using MealMind.Modules.AiChat.Application.Dtos;
 using MealMind.Modules.AiChat.Domain.Conversation;
+using MealMind.Shared.Abstractions.Kernel.CommandValidators;
 using MealMind.Shared.Abstractions.QueriesAndCommands.Commands;
 using MealMind.Shared.Abstractions.Services;
 using MealMind.Shared.Contracts.Result;
 using Microsoft.Extensions.AI;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using SharpToken;
 
 namespace MealMind.Modules.AiChat.Application.Features.Commands.CreateConversationCommand;
 
-public record CreateConversationCommand(string Prompt) : ICommand<string>
+public record CreateConversationCommand(string Prompt) : ICommand<StructuredResponse>
 {
-    public sealed class Handler : ICommandHandler<CreateConversationCommand, string>
+    public sealed class Handler : ICommandHandler<CreateConversationCommand, StructuredResponse>
     {
-        private readonly IChatClient _chatClient;
+        private readonly IAiChatService _aiChatService;
         private readonly IConversationRepository _conversationRepository;
+        private readonly IAiChatUserRepository _aiChatUserRepository;
         private readonly IUserService _userService;
+        private readonly IDocumentRepository _documentRepository;
+        private readonly IEmbeddingService _embeddingService;
         private readonly IUnitOfWork _unitOfWork;
 
-        public Handler(IChatClient chatClient, IConversationRepository conversationRepository, IUserService userService, IUnitOfWork unitOfWork)
+        public Handler(IConversationRepository conversationRepository, IAiChatUserRepository aiChatUserRepository,
+            IUserService userService,
+            IDocumentRepository documentRepository,
+            IEmbeddingService embeddingService,
+            IUnitOfWork unitOfWork, IAiChatService aiChatService)
         {
-            _chatClient = chatClient;
             _conversationRepository = conversationRepository;
+            _aiChatUserRepository = aiChatUserRepository;
             _userService = userService;
+            _documentRepository = documentRepository;
+            _embeddingService = embeddingService;
+
             _unitOfWork = unitOfWork;
+            _aiChatService = aiChatService;
         }
 
-        public async Task<Result<string>> Handle(CreateConversationCommand command, CancellationToken cancellationToken)
+        public async Task<Result<StructuredResponse>> Handle(CreateConversationCommand command, CancellationToken cancellationToken)
         {
-            if (!_userService.IsAuthenticated)
-                return Result<string>.BadRequest("User is not authenticated");
+            var aiUser = await _aiChatUserRepository.GetByUserIdAsync(_userService.UserId, cancellationToken);
+            NullValidator.ValidateNotNull(aiUser);
 
-            var conversation = Conversation.Create(_userService.UserId!.Value, "New Conversation");
-            await _conversationRepository.AddAsync(conversation, cancellationToken);
+            var userDailyPromptsCount = await _conversationRepository.GetUserDailyConversationPromptsCountAsync(aiUser.Id, cancellationToken);
 
-            var chatMessages = new List<ChatMessage>();
+            if (aiUser.DailyPromptsLimit != -1 &&
+                userDailyPromptsCount >= aiUser.DailyPromptsLimit)
+            {
+                return Result<StructuredResponse>.BadRequest("Daily prompts limit exceeded.");
+            }
 
-            // creats initial message of type ChatMessage which is type used by the IChatClient to get a response from the AI model
-            var systemInstruction = new ChatMessage(ChatRole.System,
+            var conversation = Conversation.Create(aiUser.Id, "New Conversation"); // enhance this by adding a title generation step
+
+            var systemInstruction = new ChatMessageContent(AuthorRole.System,
                 """
                 Your are a helpful assistant that helps users to plan their meals and nutrition. Provide recipes, nutritional information,
                 and meal suggestions based on user preferences and dietary needs.
                 """);
-            // creates initial message of type AiChatMessage which is type used to store messages in the database
-            var aiChatSystemMessage = AiChatMessage.Create(conversation.Id, AiChatRole.System, systemInstruction.Text, Guid.Empty);
 
+            var aiChatSystemMessage = AiChatMessage.Create(conversation.Id, AiChatRole.System, systemInstruction.Content!, Guid.Empty);
+            conversation.AddMessage(aiChatSystemMessage);
 
-            // user message - the prompt provided by the user to IChatClient
-            var userMessage = new ChatMessage(ChatRole.User, command.Prompt);
-            // user message - the prompt provided by the user to be stored in the database
+            var encoding = GptEncoding.GetEncoding("o200k_base");
+
+            if (encoding.CountTokens(command.Prompt) > aiUser.PromptTokensLimit)
+                return Result<StructuredResponse>.BadRequest("Prompt exceeds the token limit.");
+
+            var userInputEmbeddings = await _embeddingService.GenerateEmbeddingAsync(command.Prompt, cancellationToken);
+            var relevantDocuments = await _documentRepository.GetRelevantDocumentsAsync(userInputEmbeddings.ToArray(), cancellationToken);
+
+            if (!relevantDocuments.Any())
+                return Result<StructuredResponse>.BadRequest("No relevant documents found.");
+
+            var documentsText = string.Join("\n\n",
+                relevantDocuments.Select(x => $"Title: {x.Title}\nContent: {x.Content}"));
+
+            var documentTitles = relevantDocuments.Select(x => x.Title).ToList();
+
+            var userMessage = new ChatMessageContent(AuthorRole.User, command.Prompt);
             var aiChatUserMessage = AiChatMessage.Create(conversation.Id, AiChatRole.User, command.Prompt, aiChatSystemMessage.Id);
 
-            chatMessages.Add(systemInstruction);
-            chatMessages.Add(userMessage);
-
-            conversation.AddMessage(aiChatSystemMessage);
             conversation.AddMessage(aiChatUserMessage);
 
-            var response = await _chatClient.GetResponseAsync(chatMessages, cancellationToken: cancellationToken);
-            var assistantMessage = AiChatMessage.Create(conversation.Id, AiChatRole.Assistant, response.Text, aiChatUserMessage.Id);
+            var messages = new List<ChatMessageContent>
+            {
+                systemInstruction,
+                userMessage
+            };
+
+            var response = await _aiChatService.GenerateStructuredResponseAsync(
+                command.Prompt,
+                documentsText,
+                documentTitles,
+                messages,
+                aiUser.ResponseTokensLimit,
+                cancellationToken
+            );
+
+            var responseString = JsonSerializer.Serialize(response);
+
+            var assistantMessage = AiChatMessage.Create(conversation.Id, AiChatRole.Assistant, responseString, aiChatUserMessage.Id);
             conversation.AddMessage(assistantMessage);
 
+            await _conversationRepository.AddAsync(conversation, cancellationToken);
             await _unitOfWork.CommitAsync(cancellationToken);
 
-            return Result.Ok(response.Text);
+            return Result.Ok(response);
         }
     }
 }
