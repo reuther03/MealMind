fb2dcbf88ea48581810a8f9d8c8cf46de39caacb
michaldrozd <michaldrozd03@gmail.com>
2025-12-04 01:51:01 +0100

    Adds new publisher method

--- src/Services/MealMind.Services.Outbox/Database/Configurations/OutboxEventConfiguration.cs
+++ src/Services/MealMind.Services.Outbox/Database/Configurations/OutboxEventConfiguration.cs
@@ -1,6 +1,40 @@
-﻿namespace MealMind.Services.Outbox.Database.Configurations;
+﻿using MealMind.Services.Outbox.OutboxEvents;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 
-public class OutboxEventConfiguration
+namespace MealMind.Services.Outbox.Database.Configurations;
+
+public class OutboxEventConfiguration : IEntityTypeConfiguration<OutboxEvent>
 {
-    
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<OutboxEvent> builder)
+    {
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(x => x.Type)
+            .IsRequired();
+
+        var contractResolver = new DefaultContractResolver { NamingStrategy = new CamelCaseNamingStrategy() };
+        var settings = new JsonSerializerSettings
+        {
+            ContractResolver = contractResolver,
+            TypeNameHandling = TypeNameHandling.All,
+            NullValueHandling = NullValueHandling.Include
+        };
+        settings.Converters.Add(new StringEnumConverter { NamingStrategy = new CamelCaseNamingStrategy() });
+
+        builder.Property(x => x.Payload)
+            .HasColumnType("jsonb")
+            .HasConversion(
+                v => JsonConvert.SerializeObject(v, settings),
+                v => JsonConvert.DeserializeObject<object>(v, settings)!);
+
+        builder.Property(x => x.State);
+        builder.Property(x => x.CreatedOn).IsRequired();
+        builder.Property(x => x.ProcessedOn);
+    }
 }
--- src/Services/MealMind.Services.Outbox/EventMessageProcessJob.cs
+++ src/Services/MealMind.Services.Outbox/EventMessageProcessJob.cs
@@ -1,6 +1,73 @@
-﻿namespace MealMind.Services.Outbox.Database.Jobs;
+﻿using MealMind.Services.Outbox.Database;
+using MealMind.Services.Outbox.OutboxEvents;
+using MealMind.Shared.Abstractions.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-public class EventMessageProcessJob
+namespace MealMind.Services.Outbox;
+
+public class EventMessageProcessJob : BackgroundService
 {
-    
+    private readonly ILogger<EventMessageProcessJob> _logger;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly PeriodicTimer _periodicTimer = new(TimeSpan.FromSeconds(10));
+    private const int BatchSize = 20;
+
+    public EventMessageProcessJob(ILogger<EventMessageProcessJob> logger, IServiceScopeFactory serviceScopeFactory)
+    {
+        _logger = logger;
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (await _periodicTimer.WaitForNextTickAsync(stoppingToken))
+        {
+            try
+            {
+                _logger.LogInformation("Worker running on: {Name}, at: {Time}", nameof(EventMessageProcessJob), DateTime.UtcNow);
+                await ProcessEventMessageAsync(stoppingToken);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error occurred executing {Name} at {Time}", nameof(EventMessageProcessJob), DateTime.UtcNow);
+            }
+        }
+    }
+
+    private async Task ProcessEventMessageAsync(CancellationToken cancellationToken)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        // var publisher = scope.ServiceProvider.GetRequiredService<IPublisher>();
+        var outboxDbContext = scope.ServiceProvider.GetRequiredService<OutboxDbContext>();
+
+        var outboxEvents = await outboxDbContext.OutboxEvents
+            .Where(x => x.State == EventState.Pending || x.State == EventState.Failed)
+            .OrderBy(x => x.CreatedOn)
+            .Take(BatchSize)
+            .ToListAsync(cancellationToken);
+
+        if (outboxEvents.Count == 0)
+            return;
+
+        foreach (var @event in outboxEvents)
+        {
+            try
+            {
+                _logger.LogInformation("Processing event: {Id} | {Name}", @event.Id, @event.GetType().Name);
+                using var publisherScope = _serviceScopeFactory.CreateScope();
+                var publisher = scope.ServiceProvider.GetRequiredService<IPublisher>();
+                await publisher.Publish(@event.Payload, cancellationToken);
+                _logger.LogInformation("Event processed successfully: {Id} | {Name}", @event.Id, @event.GetType().Name);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        // Implementation for processing event messages goes here.
+    }
 }
