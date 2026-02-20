942ccfb2db64b6ff555e61c6865c8e9214d713c8
michaldrozd <michaldrozd03@gmail.com>
2026-02-20 19:18:52 +0100

    Adds event for food creation with ai

---
+++ src/Shared/MealMind.Shared.Events/AiChat/FoodCreatedEvent.cs
@@ -0,0 +1,7 @@
+﻿using MealMind.Shared.Abstractions.Events.Core;
+using MealMind.Shared.Abstractions.Kernel.Primitives;
+using MealMind.Shared.Contracts.Dto.Nutrition;
+
+namespace MealMind.Shared.Events.AiChat;
+
+public record FoodCreatedEvent(FoodDto Food) : Event(Guid.NewGuid());
