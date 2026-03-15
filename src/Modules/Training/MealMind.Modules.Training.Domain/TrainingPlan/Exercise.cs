9f0b1c716883a250d318e6dac492cfc9b723afc6
michaldrozd <michaldrozd03@gmail.com>
2026-03-15 02:34:41 +0100

    Adds missing constructor

--- src/Modules/Training/MealMind.Modules.Training.Domain/TrainingPlan/Exercise.cs
+++ src/Modules/Training/MealMind.Modules.Training.Domain/TrainingPlan/Exercise.cs
@@ -11,4 +11,32 @@ public class Exercise : Entity<Guid>
     public ExerciseType Type { get; private set; }
     public MuscleGroup? MuscleGroup { get; private set; }
     public bool IsCustom { get; private set; }
+
+    private Exercise()
+    {
+    }
+
+    private Exercise(string name, string description, ExerciseType type, MuscleGroup? muscleGroup, bool isCustom, string? imageUrl, string? videoUrl)
+    {
+        if (type == ExerciseType.Strength && muscleGroup == null)
+            throw new ArgumentException("Muscle group must be specified for strength exercises.", nameof(muscleGroup));
+
+        Name = name;
+        Description = description;
+        Type = type;
+        MuscleGroup = muscleGroup;
+        IsCustom = isCustom;
+        ImageUrl = imageUrl;
+        VideoUrl = videoUrl;
+    }
+
+    public static Exercise Create(
+        string name,
+        string description,
+        ExerciseType type,
+        MuscleGroup? muscleGroup,
+        bool isCustom = false,
+        string? imageUrl = null,
+        string? videoUrl = null
+    ) => new(name, description, type, muscleGroup, isCustom, imageUrl, videoUrl);
 }
