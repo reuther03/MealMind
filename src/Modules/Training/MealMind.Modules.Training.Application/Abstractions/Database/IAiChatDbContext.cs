efb7781ebdcaeb2d2bfbe495b62c5afae606ec40
michaldrozd <michaldrozd03@gmail.com>
2026-03-16 18:59:51 +0100

    Adds missing dbsets

--- src/Modules/Training/MealMind.Modules.Training.Application/Abstractions/Database/IAiChatDbContext.cs
+++ src/Modules/Training/MealMind.Modules.Training.Application/Abstractions/Database/IAiChatDbContext.cs
@@ -1,6 +1,10 @@
-﻿namespace MealMind.Modules.Training.Application.Abstractions.Database;
+﻿using MealMind.Modules.Training.Domain.TrainingPlan;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealMind.Modules.Training.Application.Abstractions.Database;
 
 public interface ITrainingDbContext
 {
-
+    DbSet<TrainingPlan> TrainingPlans { get; }
+    DbSet<Exercise> Exercises { get; }
 }
--- src/Modules/Training/MealMind.Modules.Training.Infrastructure/Database/AiChatDbContext.cs
+++ src/Modules/Training/MealMind.Modules.Training.Infrastructure/Database/AiChatDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using MealMind.Modules.Training.Application.Abstractions.Database;
+using MealMind.Modules.Training.Domain.TrainingPlan;
 using Microsoft.EntityFrameworkCore;
 
 namespace MealMind.Modules.Training.Infrastructure.Database;
 
 internal class TrainingDbContext : DbContext, ITrainingDbContext
 {
+    public DbSet<TrainingPlan> TrainingPlans => Set<TrainingPlan>();
+    public DbSet<Exercise> Exercises => Set<Exercise>();
+
     public TrainingDbContext(DbContextOptions<TrainingDbContext> options) : base(options)
     {
     }
