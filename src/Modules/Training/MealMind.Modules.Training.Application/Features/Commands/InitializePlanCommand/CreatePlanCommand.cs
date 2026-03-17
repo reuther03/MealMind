b08c8fadd3ff6d62a525f2440df5c87c133902de
michaldrozd <michaldrozd03@gmail.com>
2026-03-18 00:11:00 +0100

    Fixed property wrong name

---
+++ src/Modules/Training/MealMind.Modules.Training.Application/Features/Commands/InitializePlanCommand/CreatePlanCommand.cs
@@ -0,0 +1,32 @@
+﻿using MealMind.Modules.Training.Application.Abstractions.Database;
+using MealMind.Modules.Training.Domain.TrainingPlan;
+using MealMind.Shared.Abstractions.QueriesAndCommands.Commands;
+using MealMind.Shared.Abstractions.Services;
+using MealMind.Shared.Contracts.Result;
+
+namespace MealMind.Modules.Training.Application.Features.Commands.InitializePlanCommand;
+
+public record InitializePlanCommand : ICommand<Guid>
+{
+    public sealed class Handler : ICommandHandler<InitializePlanCommand, Guid>
+    {
+        private readonly ITrainingPlanRepository _trainingPlanRepository;
+        private readonly IUserService _userService;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Handler(ITrainingPlanRepository trainingPlanRepository, IUserService userService, IUnitOfWork unitOfWork)
+        {
+            _trainingPlanRepository = trainingPlanRepository;
+            _userService = userService;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result<Guid>> Handle(InitializePlanCommand command, CancellationToken cancellationToken)
+        {
+            if (!_userService.IsAuthenticated)
+                return Result<Guid>.BadRequest("User is not authenticated.");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Modules/Training/MealMind.Modules.Training.Infrastructure/Database/Migrations/20260317230404_FixedTypo.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MealMind.Modules.Training.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlanedOn",
+                schema: "training",
+                table: "TrainingPlan",
+                newName: "PlannedOn");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlannedOn",
+                schema: "training",
+                table: "TrainingPlan",
+                newName: "PlanedOn");
+        }
+    }
+}
