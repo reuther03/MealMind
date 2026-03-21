8372addf9cd7c3a11b18c0be1b52b1b735583bda
michaldrozd <michaldrozd03@gmail.com>
2026-03-22 00:59:08 +0100

    Adds GetExerciseQuery

---
+++ src/Modules/Training/MealMind.Modules.Training.Application/Features/Queries/GetExerciseQuery.cs
@@ -0,0 +1,41 @@
+﻿using MealMind.Modules.Training.Application.Abstractions.Database;
+using MealMind.Shared.Abstractions.QueriesAndCommands.Queries;
+using MealMind.Shared.Contracts.Dto.Training;
+using MealMind.Shared.Contracts.Result;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealMind.Modules.Training.Application.Features.Queries;
+
+public record GetExerciseQuery(Guid ExerciseId) : IQuery<ExerciseDto>
+{
+    public sealed class Handler : IQueryHandler<GetExerciseQuery, ExerciseDto>
+    {
+        private readonly ITrainingDbContext _dbContext;
+
+        public Handler(ITrainingDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Result<ExerciseDto>> Handle(GetExerciseQuery query, CancellationToken cancellationToken = default)
+        {
+            var exercise = await _dbContext.Exercises.FirstOrDefaultAsync(x => x.Id == query.ExerciseId, cancellationToken);
+            if (exercise == null)
+                return Result<ExerciseDto>.BadRequest("Exercise not found.");
+
+            var exerciseDto = new ExerciseDto
+            {
+                Id = exercise.Id,
+                Name = exercise.Name,
+                Description = exercise.Description,
+                ImageUrl = exercise.ImageUrl,
+                VideoUrl = exercise.VideoUrl,
+                Type = exercise.Type.ToString(),
+                MuscleGroup = exercise.MuscleGroup?.ToString(),
+                IsCustom = exercise.IsCustom
+            };
+
+            return Result<ExerciseDto>.Ok(exerciseDto);
+        }
+    }
+}
---
+++ src/Shared/MealMind.Shared.Contracts/Dto/Training/ExerciseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MealMind.Shared.Contracts.Dto.Training;
+
+public class ExerciseDto
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; } = null!;
+    public string Description { get; init; } = null!;
+    public string? ImageUrl { get; init; }
+    public string? VideoUrl { get; init; }
+    public string Type { get; init; } = null!;
+    public string? MuscleGroup { get; init; }
+    public bool IsCustom { get; init; }
+}
