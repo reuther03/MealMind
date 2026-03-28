bdfa77c06af63d5f74ca339f0cda22093d8a7f75
michaldrozd <michaldrozd03@gmail.com>
2026-03-28 15:47:41 +0100

    Separate tests by parameters

--- tests/Modules/Nutrition/Tests/MealMind.Modules.Nutrition.Tests.Unit/PersonalDataTest.cs
+++ tests/Modules/Nutrition/Tests/MealMind.Modules.Nutrition.Tests.Unit/PersonalDataTest.cs
@@ -22,41 +22,113 @@ public class PersonalDataTest
     }
 
     [Test]
-    public async Task Create_InvalidWeightAndHeight_ShouldThrow()
+    public async Task Create_FutureDateOfBirth_ShouldThrow()
     {
         var gender = Gender.Male;
-        var dateOfBirth = DateOnly.FromDateTime(DateTime.Now.AddYears(-30));
-        var invalidDateOfBirth = DateOnly.FromDateTime(DateTime.Now.AddYears(1));
-        var validWeight = 50;
-        var invalidWeightToSmall = 8;
-        var invalidWeightToBig = 500;
+        var dateOfBirth = DateOnly.FromDateTime(DateTime.Now.AddYears(1));
+        var weight = 50;
         var height = 180;
-        var invalidHeight = 300;
         var weightTarget = 60;
         var activityLevel = ActivityLevel.Active;
 
 
-        await Assert.That(() => PersonalData.Create(gender, invalidDateOfBirth, validWeight, height, weightTarget, activityLevel))
+        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, weight, height, weightTarget, activityLevel))
             .Throws<DomainException>()
             .WithMessage("Date of birth cannot be in the future");
+    }
 
-        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, invalidWeightToSmall, height, weightTarget, activityLevel))
+    [Test]
+    public async Task Create_WeightTooSmall_ShouldThrow()
+    {
+        var gender = Gender.Male;
+        var dateOfBirth = DateOnly.FromDateTime(DateTime.Now.AddYears(-30));
+        var weight = 9;
+        var height = 180;
+        var weightTarget = 60;
+        var activityLevel = ActivityLevel.Active;
+
+
+        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, weight, height, weightTarget, activityLevel))
             .Throws<DomainException>()
             .WithMessage("Weight must be between 10 and 400");
+    }
 
-        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, invalidWeightToBig, height, weightTarget, activityLevel))
+    [Test]
+    public async Task Create_WeightTooBig_ShouldThrow()
+    {
+        var gender = Gender.Male;
+        var dateOfBirth = DateOnly.FromDateTime(DateTime.Now.AddYears(-30));
+        var weight = 500;
+        var height = 180;
+        var weightTarget = 60;
+        var activityLevel = ActivityLevel.Active;
+
+
+        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, weight, height, weightTarget, activityLevel))
             .Throws<DomainException>()
             .WithMessage("Weight must be between 10 and 400");
+    }
 
-        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, validWeight, height, invalidWeightToSmall, activityLevel))
+    [Test]
+    public async Task Create_WeightTargetTooSmall_ShouldThrow()
+    {
+        var gender = Gender.Male;
+        var dateOfBirth = DateOnly.FromDateTime(DateTime.Now.AddYears(-30));
+        var weight = 50;
+        var height = 180;
+        var weightTarget = 9;
+        var activityLevel = ActivityLevel.Active;
+
+
+        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, weight, height, weightTarget, activityLevel))
             .Throws<DomainException>()
             .WithMessage("Weight must be between 10 and 400");
+    }
 
-        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, validWeight, height, invalidWeightToBig, activityLevel))
+    [Test]
+    public async Task Create_WeightTargetTooBig_ShouldThrow()
+    {
+        var gender = Gender.Male;
+        var dateOfBirth = DateOnly.FromDateTime(DateTime.Now.AddYears(-30));
+        var weight = 50;
+        var height = 180;
+        var weightTarget = 600;
+        var activityLevel = ActivityLevel.Active;
+
+
+        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, weight, height, weightTarget, activityLevel))
             .Throws<DomainException>()
             .WithMessage("Weight must be between 10 and 400");
+    }
 
-        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, validWeight, invalidHeight, validWeight, activityLevel))
+    [Test]
+    public async Task Create_HeightTooSmall_ShouldThrow()
+    {
+        var gender = Gender.Male;
+        var dateOfBirth = DateOnly.FromDateTime(DateTime.Now.AddYears(-30));
+        var weight = 50;
+        var height = 79;
+        var weightTarget = 60;
+        var activityLevel = ActivityLevel.Active;
+
+
+        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, weight, height, weightTarget, activityLevel))
+            .Throws<DomainException>()
+            .WithMessage("Height must be between 80cm and 250cm");
+    }
+
+    [Test]
+    public async Task Create_HeightTooTall_ShouldThrow()
+    {
+        var gender = Gender.Male;
+        var dateOfBirth = DateOnly.FromDateTime(DateTime.Now.AddYears(-30));
+        var weight = 50;
+        var height = 251;
+        var weightTarget = 60;
+        var activityLevel = ActivityLevel.Active;
+
+
+        await Assert.That(() => PersonalData.Create(gender, dateOfBirth, weight, height, weightTarget, activityLevel))
             .Throws<DomainException>()
             .WithMessage("Height must be between 80cm and 250cm");
     }
