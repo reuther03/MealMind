347149d0e25c4e30f5491ae23cd33c4b8c7bcd38
michaldrozd <michaldrozd03@gmail.com>
2026-03-28 20:16:02 +0100

    Added Handler_InvalidEmail_ShouldReturnBadRequest test for signup

--- tests/Modules/Identity/Tests/MealMind.Modules.Identity.Tests.Unit/User/SignUpCommandTest.cs
+++ tests/Modules/Identity/Tests/MealMind.Modules.Identity.Tests.Unit/User/SignUpCommandTest.cs
@@ -19,7 +19,7 @@ public class SignUpCommandTest
 
 
     [Test]
-    public async Task Handler_ValidData_ShouldSignUpSuccessfully()
+    public async Task Handler_ValidData_ShouldSignUp()
     {
         var username = "Test User";
         var email = "test@test.com";
@@ -58,5 +58,51 @@ public class SignUpCommandTest
         _unitOfWork.Verify(x => x.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
         _outboxService.Verify(x => x.SaveAsync(It.IsAny<IdentityUserCreatedEvent>(), It.IsAny<CancellationToken>()), Times.Once);
         _outboxService.Verify(x => x.SaveAsync(It.IsAny<SubscriptionTierAddedEvent>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Test]
+    public async Task Handler_InvalidEmail_ShouldReturnBadRequest()
+    {
+        var username = "Test User";
+        var email = "test@test.com";
+        var password = "Test123!";
+
+        var nutritionTargets = new List<NutritionTargetPayload>
+        {
+            new(2000, null, new NutritionInPercentPayload
+            {
+                ProteinInPercent = 30,
+                CarbohydratesInPercent = 30,
+                FatsInPercent = 40
+            }, 2, [DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday]),
+            new(2200, null, new NutritionInPercentPayload
+            {
+                ProteinInPercent = 30,
+                CarbohydratesInPercent = 30,
+                FatsInPercent = 40
+            }, 2.5m, [DayOfWeek.Thursday, DayOfWeek.Friday]),
+            new(2500, null, new NutritionInPercentPayload
+            {
+                ProteinInPercent = 30,
+                CarbohydratesInPercent = 30,
+                FatsInPercent = 40
+            }, 3, [DayOfWeek.Saturday, DayOfWeek.Sunday])
+        };
+        var personalData = new PersonalDataPayload(Gender.Male, DateOnly.FromDateTime(DateTime.Now.AddYears(-30)), 180, 80, 75, ActivityLevel.Active);
+
+        var commandPayload = new SignUpCommand(username, email, password, personalData, nutritionTargets);
+
+        _identityUserRepository
+            .Setup(x => x.ExistsWithEmailAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        var commandHandler = new SignUpCommand.Handler(_identityUserRepository.Object, _unitOfWork.Object, _outboxService.Object);
+        await commandHandler.Handle(commandPayload, CancellationToken.None);
+
+        _identityUserRepository.Verify(x => x.ExistsWithEmailAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Once);
+        _identityUserRepository.Verify(x => x.AddAsync(It.IsAny<IdentityUser>(), It.IsAny<CancellationToken>()), Times.Never);
+        _unitOfWork.Verify(x => x.CommitAsync(It.IsAny<CancellationToken>()), Times.Never);
+        _outboxService.Verify(x => x.SaveAsync(It.IsAny<IdentityUserCreatedEvent>(), It.IsAny<CancellationToken>()), Times.Never);
+        _outboxService.Verify(x => x.SaveAsync(It.IsAny<SubscriptionTierAddedEvent>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 }
