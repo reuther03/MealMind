e5101f44ac13e57d443e84a3bf68e507eecba1e8
michaldrozd <michaldrozd03@gmail.com>
2026-03-28 16:18:08 +0100

    Adds Handler_ValidData_ShouldSignUpSuccessfully

--- tests/Modules/Identity/Tests/MealMind.Modules.Identity.Tests.Unit/User/SignUpCommandTest.cs
+++ tests/Modules/Identity/Tests/MealMind.Modules.Identity.Tests.Unit/User/SignUpCommandTest.cs
@@ -2,7 +2,11 @@
 using MealMind.Modules.Identity.Application.Abstractions.Database;
 using MealMind.Modules.Identity.Application.Features.Commands.SignUpCommand;
 using MealMind.Modules.Identity.Domain.IdentityUser;
+using MealMind.Shared.Abstractions.Kernel.ValueObjects;
 using MealMind.Shared.Abstractions.Services;
+using MealMind.Shared.Contracts.Dto.Nutrition;
+using MealMind.Shared.Contracts.Types;
+using MealMind.Shared.Events.Identity;
 using Moq;
 
 namespace MealMind.Modules.Identity.Tests.Unit.User;
@@ -17,15 +21,42 @@ public class SignUpCommandTest
     [Test]
     public async Task Handler_ValidData_ShouldSignUpSuccessfully()
     {
-        // var username = "Test User";
-        // var email = "test@test.com";
-        // var password = "Test123!";
-        //
-        // var user = IdentityUser.Create(username, email, password);
-        //
-        // var commandHandler = new SignUpCommand.Handler(_identityUserRepository.Object, _unitOfWork.Object, _outboxService.Object);
-        // await eventHandler.Handle(eventPayload, CancellationToken.None);
-        //
-        // _userProfileRepository.Verify(x => x.AddAsync(It.IsAny<UserProfile>(), It.IsAny<CancellationToken>()), Times.Once);
+        var username = "Test User";
+        var email = "test@test.com";
+        var password = "Test123!";
+
+        var nutritionTargets = new List<NutritionTargetPayload>
+        {
+            new(2000, null, new NutritionInPercentPayload
+            {
+                ProteinInPercent = 30,
+                CarbohydratesInPercent = 30,
+                FatsInPercent = 40
+            }, 2, [DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday]),
+            new(2200, null, new NutritionInPercentPayload
+            {
+                ProteinInPercent = 30,
+                CarbohydratesInPercent = 30,
+                FatsInPercent = 40
+            }, 2.5m, [DayOfWeek.Thursday, DayOfWeek.Friday]),
+            new(2500, null, new NutritionInPercentPayload
+            {
+                ProteinInPercent = 30,
+                CarbohydratesInPercent = 30,
+                FatsInPercent = 40
+            }, 3, [DayOfWeek.Saturday, DayOfWeek.Sunday])
+        };
+        var personalData = new PersonalDataPayload(Gender.Male, DateOnly.FromDateTime(DateTime.Now.AddYears(-30)), 180, 80, 75, ActivityLevel.Active);
+
+        var commandPayload = new SignUpCommand(username, email, password, personalData, nutritionTargets);
+
+        var commandHandler = new SignUpCommand.Handler(_identityUserRepository.Object, _unitOfWork.Object, _outboxService.Object);
+        await commandHandler.Handle(commandPayload, CancellationToken.None);
+
+        _identityUserRepository.Verify(x => x.ExistsWithEmailAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Once);
+        _identityUserRepository.Verify(x => x.AddAsync(It.IsAny<IdentityUser>(), It.IsAny<CancellationToken>()), Times.Once);
+        _unitOfWork.Verify(x => x.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
+        _outboxService.Verify(x => x.SaveAsync(It.IsAny<IdentityUserCreatedEvent>(), It.IsAny<CancellationToken>()), Times.Once);
+        _outboxService.Verify(x => x.SaveAsync(It.IsAny<SubscriptionTierAddedEvent>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 }
