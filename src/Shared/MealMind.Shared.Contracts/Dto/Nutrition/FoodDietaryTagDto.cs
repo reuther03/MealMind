0531cca4a797448c04fc5eba58fe27661955e43b
michaldrozd <michaldrozd03@gmail.com>
2026-04-28 15:44:21 +0200

    Solution clean up

--- src/Shared/MealMind.Shared.Contracts/Dto/Nutrition/FoodDietaryTagDto.cs
+++
@@ -1,7 +0,0 @@
-namespace MealMind.Shared.Contracts.Dto.Nutrition;
-
-public sealed class FoodDietaryTagDto
-{
-    public Guid Id { get; init; }
-    public string DietaryTag { get; init; } = null!;
-}
