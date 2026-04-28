ca0153250d4e0622d541cddda6e1a66273c0c10e
michaldrozd <michaldrozd03@gmail.com>
2026-04-28 15:42:14 +0200

    Refactores FoodDto

--- src/Shared/MealMind.Shared.Contracts/Dto/Nutrition/FoodDto.cs
+++ src/Shared/MealMind.Shared.Contracts/Dto/Nutrition/FoodDto.cs
@@ -1,14 +1,14 @@
 namespace MealMind.Shared.Contracts.Dto.Nutrition;
 
-public class FoodDto
+public record FoodDto
 {
     public Guid? Id { get; init; }
     public string Name { get; init; } = null!;
     public string? Barcode { get; init; }
     public string? Brand { get; init; }
     public string? ImageUrl { get; init; }
-    public List<FoodCategoryDto>? Categories { get; init; }
-    public List<FoodDietaryTagDto>? DietaryTags { get; init; }
+    public List<string>? Categories { get; init; }
+    public List<string>? DietaryTags { get; init; }
     public NutrimentsPer100GDto NutritionPer100G { get; init; } = null!;
     public DateTime CreatedAt { get; init; }
     public FoodDataSource FoodSource { get; init; }
