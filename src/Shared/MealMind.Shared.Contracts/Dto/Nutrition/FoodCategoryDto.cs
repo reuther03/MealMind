9b22930bcfd8b62d9d9f7bcb9d8e39307de96c3c
michaldrozd <michaldrozd03@gmail.com>
2026-04-28 15:44:21 +0200

    Solution clean up

--- src/Shared/MealMind.Shared.Contracts/Dto/Nutrition/FoodCategoryDto.cs
+++
@@ -1,7 +0,0 @@
-namespace MealMind.Shared.Contracts.Dto.Nutrition;
-
-public sealed class FoodCategoryDto
-{
-    public Guid Id { get; init; }
-    public string Category { get; init; } = null!;
-}
