6714fbb4b11236756127b2dee48a7e1c39c56f25
michaldrozd <michaldrozd03@gmail.com>
2026-06-02 23:57:00 +0200

    Adds missing validation to ExerciesSet

--- src/Modules/Training/MealMind.Modules.Training.Domain/TrainingPlan/ExerciseSet.cs
+++ src/Modules/Training/MealMind.Modules.Training.Domain/TrainingPlan/ExerciseSet.cs
@@ -1,4 +1,5 @@
-﻿using MealMind.Shared.Abstractions.Kernel.Primitives;
+﻿using MealMind.Shared.Abstractions.Exception;
+using MealMind.Shared.Abstractions.Kernel.Primitives;
 
 namespace MealMind.Modules.Training.Domain.TrainingPlan;
 
@@ -12,6 +13,15 @@ public record ExerciseSet : ValueObject
 
     public ExerciseSet(int setNumber, int repetitions, decimal weight, SetType setType, int? restTimeInSeconds = null)
     {
+        if (restTimeInSeconds <= 0)
+            throw new DomainException("Rest time must be a positive integer.", nameof(restTimeInSeconds));
+
+        if (repetitions <= 0)
+            throw new DomainException("Repetitions must be a positive integer.", nameof(repetitions));
+
+        if (weight < 0)
+            throw new DomainException("Weight must be a positive integer.", nameof(weight));
+
         SetNumber = setNumber;
         Repetitions = repetitions;
         Weight = weight;
