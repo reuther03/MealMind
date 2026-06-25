6fef3fe4c716dd990c6ac3dbb1b0970210ba074b
michaldrozd <michaldrozd03@gmail.com>
2026-06-25 19:09:13 +0200

    Deletes TrainingSummaryService

--- src/Shared/MealMind.Shared.Abstractions/Services/ITrainingSummaryService.cs
+++
@@ -1,8 +0,0 @@
-﻿using MealMind.Shared.Abstractions.Kernel.ValueObjects.Ids;
-
-namespace MealMind.Shared.Abstractions.Services;
-
-public interface ITrainingSummaryService
-{
-    Task<string> BuildSummaryAsync(UserId userId, CancellationToken ct);
-}
